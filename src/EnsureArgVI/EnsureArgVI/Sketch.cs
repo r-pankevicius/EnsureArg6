884d1871940ced1e7695ef95515df5a7e9051be3
Remigijus Pankevičius <r-pankevicius@users.noreply.github.com>
2023-05-04 23:09:14 +0300

    Added Sketch

---
+++ src/EnsureArgVI/EnsureArgVI/Sketch.cs
@@ -0,0 +1,19 @@
+﻿using EnsureThat;
+
+namespace EnsureArgVI
+{
+	public class Sketch
+	{
+		public int Zero => 0;
+
+		public void CheckNotNull(string? value)
+		{
+			EnsureArg.IsNotNull(value);
+		}
+
+		public void CheckNotNullWithNameof(string? value)
+		{
+			EnsureArg.IsNotNull(value, nameof(value));
+		}
+	}
+}
