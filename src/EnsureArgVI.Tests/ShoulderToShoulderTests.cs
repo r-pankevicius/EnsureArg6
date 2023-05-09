88ab50e194b0e5533aa08704021f93d56ac31e26
Remigijus Pankevičius <r-pankevicius@users.noreply.github.com>
2023-05-09 22:15:34 +0300

    ShoulderToShoulderTests frame

---
+++ src/EnsureArgVI.Tests/ShoulderToShoulderTests.cs
@@ -0,0 +1,9 @@
+﻿namespace EnsureArgVI.Tests
+{
+	/// <summary>
+	/// Inspired by Lithuanian PM Algirdas Butkevičius: https://youtu.be/o14-cvvN1_w
+	/// </summary>
+	public class ShoulderToShoulderTests
+	{
+	}
+}
