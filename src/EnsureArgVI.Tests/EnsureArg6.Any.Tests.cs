ecab7197dd7dcf4b11fb4ca8d44aa04f3f1dc58f
Remigijus Pankevičius <r-pankevicius@users.noreply.github.com>
2023-05-10 00:15:45 +0300

    Added TestBase

--- src/EnsureArgVI.Tests/EnsureArg6.Any.Tests.cs
+++ src/EnsureArgVI.Tests/EnsureArg6.Any.Tests.cs
@@ -1,5 +1,4 @@
 ﻿using EnsureThat;
-using System.Diagnostics.CodeAnalysis;
 
 namespace EnsureArgVI.Tests
 {
@@ -11,47 +10,10 @@ namespace EnsureArgVI.Tests
 			static void NoNullzPlz(object param78y653) => EnsureArg.IsNotNull(param78y653, nameof(param78y653));
 			static void NoNullzPlz6(object param78y653) => EnsureArg6.IsNotNull(param78y653);
 
+			//!CS8622
 			// Exclamation marks are used to suppress the warning about the parameter being null.
 			// What is strange because compiler sees that the parameter is not null.
-			AssertThrowsTheSameArgumentException(NoNullzPlz!, NoNullzPlz6!);
-		}
-
-		private static void AssertThrowsTheSameArgumentException(
-			[NotNull] Action<object?> ensureArgAction,
-			[NotNull] Action<object?> ensureArg6Action)
-		{
-			Exception? ensureArgException = null;
-			Exception? ensureArg6Exception = null;
-
-			try
-			{
-				ensureArgAction(null);
-			}
-			catch (ArgumentException ex1)
-			{
-				ensureArgException = ex1;
-			}
-
-			if (ensureArgException is null)
-			{
-				throw new InvalidOperationException($"{nameof(ensureArgAction)} did not throw.");
-			}
-
-			try
-			{
-				ensureArg6Action(null);
-			}
-			catch (ArgumentException ex1)
-			{
-				ensureArg6Exception = ex1;
-			}
-
-			if (ensureArg6Exception is null)
-			{
-				throw new InvalidOperationException($"{nameof(ensureArg6Action)} did not throw.");
-			}
-
-			Assert.Equal(ensureArgException.Message, ensureArg6Exception.Message);
+			TestBase.AssertThrowsTheSameArgumentException(NoNullzPlz!, NoNullzPlz6!);
 		}
 	}
 }
---
+++ src/EnsureArgVI.Tests/TestBase.cs
@@ -0,0 +1,45 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace EnsureArgVI.Tests
+{
+	internal static class TestBase
+	{
+		internal static void AssertThrowsTheSameArgumentException(
+			[NotNull] Action<object?> ensureArgAction,
+			[NotNull] Action<object?> ensureArg6Action)
+		{
+			Exception? ensureArgException = null;
+			Exception? ensureArg6Exception = null;
+
+			try
+			{
+				ensureArgAction(null);
+			}
+			catch (ArgumentException ex1)
+			{
+				ensureArgException = ex1;
+			}
+
+			if (ensureArgException is null)
+			{
+				throw new InvalidOperationException($"{nameof(ensureArgAction)} did not throw.");
+			}
+
+			try
+			{
+				ensureArg6Action(null);
+			}
+			catch (ArgumentException ex1)
+			{
+				ensureArg6Exception = ex1;
+			}
+
+			if (ensureArg6Exception is null)
+			{
+				throw new InvalidOperationException($"{nameof(ensureArg6Action)} did not throw.");
+			}
+
+			Assert.Equal(ensureArgException.Message, ensureArg6Exception.Message);
+		}
+	}
+}
