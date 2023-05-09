43b9e57d0b5b6ce0624a6be8236fa1f4b1789ca8
Remigijus Pankevičius <r-pankevicius@users.noreply.github.com>
2023-05-09 21:52:35 +0300

    First port

--- src/EnsureArgVI/EnsureArg6.Any.cs
+++ src/EnsureArgVI/EnsureArg6.Any.cs
@@ -1,11 +1,15 @@
-﻿using System.Diagnostics.CodeAnalysis;
+﻿using EnsureThat;
+using System.Diagnostics.CodeAnalysis;
+using System.Runtime.CompilerServices;
 
 namespace EnsureArgVI
 {
 	public static partial class EnsureArg6
 	{
         [return: NotNull]
-        public static T IsNotNull<T>([NotNull] T value, [InvokerParameterName] string paramName = null) where T : class
-            => Ensure.Any.IsNotNull(value, paramName);
+        public static T IsNotNull<T>(
+            [NotNull] T value,
+			[CallerArgumentExpression("value")] string paramName = "") where T : class
+            => EnsureArg.IsNotNull(value, paramName);
     }
 }
