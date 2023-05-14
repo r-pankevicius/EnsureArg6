564a9f1aabc8b8a70f0cd59c8df57025d57897d4
Remigijus Pankevičius <r-pankevicius@users.noreply.github.com>
2023-05-14 23:13:00 +0300

    EnsureArg6.ValueTypes

--- src/EnsureArgVI/EnsureArg6.ValueTypes.cs
+++ src/EnsureArgVI/EnsureArg6.ValueTypes.cs
@@ -1,25 +1,25 @@
-﻿#if false
+﻿using EnsureThat;
 using System;
+using System.Runtime.CompilerServices;
 
 namespace EnsureArgVI
 {
     // https://github.com/danielwertheim/Ensure.That/blob/master/src/projects/EnsureThat/EnsureArg.ValueTypes.cs
     public static partial class EnsureArg6
     {
-        public static bool IsTrue(bool value, [InvokerParameterName] string paramName = null)
-            => Ensure.Bool.IsTrue(value, paramName);
+        public static bool IsTrue(bool value, [CallerArgumentExpression("value")] string paramName = "")
+            => EnsureArg.IsTrue(value, paramName);
 
-        public static bool IsFalse(bool value, [InvokerParameterName] string paramName = null)
-            => Ensure.Bool.IsFalse(value, paramName);
+        public static bool IsFalse(bool value, [CallerArgumentExpression("value")] string paramName = "")
+            => EnsureArg.IsFalse(value, paramName);
 
-        public static T IsNotDefault<T>(T value, [InvokerParameterName] string paramName = null) where T : struct
+        public static T IsNotDefault<T>(T value, [CallerArgumentExpression("value")] string paramName = "") where T : struct
             => Ensure.Any.IsNotDefault(value, paramName);
 
-        public static T? IsNotNull<T>(T? value, [InvokerParameterName] string paramName = null) where T : struct
+        public static T? IsNotNull<T>(T? value, [CallerArgumentExpression("value")] string paramName = "") where T : struct
             => Ensure.Any.IsNotNull(value, paramName);
 
-        public static Guid IsNotEmpty(Guid value, [InvokerParameterName] string paramName = null)
+        public static Guid IsNotEmpty(Guid value, [CallerArgumentExpression("value")] string paramName = "")
             => Ensure.Guid.IsNotEmpty(value, paramName);
     }
 }
-#endif
