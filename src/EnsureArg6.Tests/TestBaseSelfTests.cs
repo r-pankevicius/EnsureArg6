686122515ca1bc80eb636f8d04490dfea47b2a26
Remigijus Pankevičius <r-pankevicius@users.noreply.github.com>
2023-05-17 20:48:08 +0300

    TestBaseSelfTests

---
+++ src/EnsureArg6.Tests/TestBaseSelfTests.cs
@@ -0,0 +1,56 @@
+﻿using Xunit.Sdk;
+
+namespace EnsureArg6.Tests
+{
+	public class TestBaseSelfTests
+	{
+		[Fact]
+		public void AssertThrowsTheSameArgumentException_DoesntThrow_OnSameException()
+		{
+			static int Check1(int _param) => throw new ArgumentException("qwerty");
+			static int Check2(int _param) => throw new ArgumentException("qwerty");
+
+			TestBase.AssertThrowsTheSameArgumentException(42, Check1, Check2);
+		}
+
+		[Fact]
+		public void AssertThrowsTheSameArgumentException_Throws_OnDifferentExceptionType()
+		{
+			static int Check1(int _param) => throw new ArgumentException("qwerty");
+			static int Check2(int _param) => throw new InvalidOperationException("abcedf");
+
+			bool exceptionWasThrown = false;
+
+			try
+			{
+				TestBase.AssertThrowsTheSameArgumentException(42, Check1, Check2);
+			}
+			catch (InvalidOperationException)
+			{
+				exceptionWasThrown = true;
+			}
+
+			exceptionWasThrown.Should().BeTrue();
+		}
+
+		[Fact]
+		public void AssertThrowsTheSameArgumentException_Throws_OnDifferentExceptionMessage()
+		{
+			static int Check1(int _param) => throw new ArgumentException("qwerty");
+			static int Check2(int _param) => throw new ArgumentException("abcedf");
+
+			bool exceptionWasThrown = false;
+
+			try
+			{
+				TestBase.AssertThrowsTheSameArgumentException(42, Check1, Check2);
+			}
+			catch (XunitException)
+			{
+				exceptionWasThrown = true;
+			}
+
+			exceptionWasThrown.Should().BeTrue();
+		}
+	}
+}
