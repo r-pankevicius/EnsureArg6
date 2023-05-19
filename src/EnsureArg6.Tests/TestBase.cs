651d0361c2143c05e4d76ca889afad437fd45c88
Remigijus Pankevičius <r-pankevicius@users.noreply.github.com>
2023-05-19 23:10:26 +0300

    Deleted something

--- src/EnsureArg6.Tests/TestBase.cs
+++ src/EnsureArg6.Tests/TestBase.cs
@@ -8,43 +8,8 @@
 			Func<T, T> ensureArg6Func)
 		{
 			AssertThrowsTheSameArgumentException_Impl(
-				paramAndRetVal,
-				(arg) => { _ = ensureArgFunc(arg); },
-				(arg) => { _ = ensureArg6Func(arg); });
-		}
-
-		private static void AssertThrowsTheSameArgumentException_Impl<T>(
-			T param,
-			Action<T> ensureArgAction,
-			Action<T> ensureArg6Action)
-		{
-			Exception? ensureArgException = null;
-			Exception? ensureArg6Exception = null;
-
-			try
-			{
-				ensureArgAction(param);
-			}
-			catch (ArgumentException ex1)
-			{
-				ensureArgException = ex1;
-			}
-
-			ensureArgException.Should().NotBeNull($"{nameof(ensureArgAction)} did not throw.");
-
-			try
-			{
-				ensureArg6Action(param);
-			}
-			catch (ArgumentException ex1)
-			{
-				ensureArg6Exception = ex1;
-			}
-
-			ensureArg6Exception.Should().NotBeNull($"{nameof(ensureArg6Action)} did not throw.");
-
-			ensureArgException!.Should().BeOfType(ensureArg6Exception!.GetType(), $"{nameof(ensureArgAction)} and {nameof(ensureArg6Action)} did not throw the same exception type.");
-			ensureArgException!.Message.Should().Be(ensureArg6Exception.Message, $"{nameof(ensureArgAction)} and {nameof(ensureArg6Action)} did not throw the same exception message.");
+				() => { _ = ensureArgFunc(paramAndRetVal); },
+				() => { _ = ensureArg6Func(paramAndRetVal); });
 		}
 
 		internal static void AssertThrowsTheSameArgumentException<T>(
