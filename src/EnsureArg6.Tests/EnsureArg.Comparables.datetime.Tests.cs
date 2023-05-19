11d3ccb6942ae1254a992c187aeaba679d621e4c
Remigijus Pankevičius <r-pankevicius@users.noreply.github.com>
2023-05-19 23:24:11 +0300

    EnsureArg6_Comparables_datetime_Tests done except InRange

--- src/EnsureArg6.Tests/EnsureArg.Comparables.datetime.Tests.cs
+++ src/EnsureArg6.Tests/EnsureArg.Comparables.datetime.Tests.cs
@@ -4,16 +4,16 @@ namespace EnsureArg6.Tests
 {
 	public class EnsureArg6_Comparables_datetime_Tests
 	{
-		private static readonly DateTime Before = new DateTime(2000, 1, 1);
-		private static readonly DateTime After = new DateTime(2023, 12, 31);
+		private static readonly DateTime Before = new (2000, 1, 1);
+		private static readonly DateTime After = new (2023, 12, 31);
 
 		[Fact]
 		public void Is_Test()
 		{
-			static DateTime Check(DateTime myParamPamPam) => EnsureArg.Is(myParamPamPam, myParamPamPam, nameof(myParamPamPam));
-			static DateTime Check6(DateTime myParamPamPam) => EnsureArg6.Is(myParamPamPam, myParamPamPam);
-			
-			TestBase.AssertThrowsTheSameArgumentException(DateTime.Now, Check, Check6);
+			static DateTime Check(DateTime param1, DateTime param2) => EnsureArg.Is(param1, param2, nameof(param1));
+			static DateTime Check6(DateTime param1, DateTime param2) => EnsureArg6.Is(param1, param2);
+
+			TestBase.AssertThrowsTheSameArgumentException(Before, After, Check, Check6);
 		}
 
 		[Fact]
@@ -22,51 +22,54 @@ namespace EnsureArg6.Tests
 			static DateTime Check(DateTime param1, DateTime param2) => EnsureArg.IsNot(param1, param2, nameof(param1));
 			static DateTime Check6(DateTime param1, DateTime param2) => EnsureArg6.IsNot(param1, param2);
 
-			TestBase.AssertThrowsTheSameArgumentException(Before, After, Check, Check6);
+			TestBase.AssertThrowsTheSameArgumentException(Before, Before, Check, Check6);
 		}
 
 		[Fact]
 		public void IsLt_Test()
 		{
-			static DateTime Check(DateTime myParamPamPam) => EnsureArg.IsLt(myParamPamPam, myParamPamPam, nameof(myParamPamPam));
-			static DateTime Check6(DateTime myParamPamPam) => EnsureArg6.IsLt(myParamPamPam, myParamPamPam);
-			TestBase.AssertThrowsTheSameArgumentException(DateTime.Now, Check, Check6);
+			static DateTime Check(DateTime param1, DateTime param2) => EnsureArg.IsLt(param1, param2, nameof(param1));
+			static DateTime Check6(DateTime param1, DateTime param2) => EnsureArg6.IsLt(param1, param2);
+
+			TestBase.AssertThrowsTheSameArgumentException(Before, Before, Check, Check6);
 		}
 
 		[Fact]
 		public void IsLte_Test()
 		{
-			static DateTime Check(DateTime myParamPamPam) => EnsureArg.IsLte(myParamPamPam, myParamPamPam, nameof(myParamPamPam));
-			static DateTime Check6(DateTime myParamPamPam) => EnsureArg6.IsLte(myParamPamPam, myParamPamPam);
-			TestBase.AssertThrowsTheSameArgumentException(DateTime.Now, Check, Check6);
+			static DateTime Check(DateTime param1, DateTime param2) => EnsureArg.IsLte(param1, param2, nameof(param1));
+			static DateTime Check6(DateTime param1, DateTime param2) => EnsureArg6.IsLte(param1, param2);
+
+			TestBase.AssertThrowsTheSameArgumentException(After, Before, Check, Check6);
 		}
 
 		[Fact]
 		public void IsGt_Test()
 		{
-			static DateTime Check(DateTime myParamPamPam) => EnsureArg.IsGt(myParamPamPam, myParamPamPam, nameof(myParamPamPam));
-			static DateTime Check6(DateTime myParamPamPam) => EnsureArg6.IsGt(myParamPamPam, myParamPamPam);
-			
-			TestBase.AssertThrowsTheSameArgumentException(DateTime.Now, Check, Check6);
+			static DateTime Check(DateTime param1, DateTime param2) => EnsureArg.IsGt(param1, param2, nameof(param1));
+			static DateTime Check6(DateTime param1, DateTime param2) => EnsureArg6.IsGt(param1, param2);
+
+			TestBase.AssertThrowsTheSameArgumentException(Before, Before, Check, Check6);
 		}
 
 		[Fact]
 		public void IsGte_Test()
 		{
-			static DateTime Check(DateTime myParamPamPam) => EnsureArg.IsGte(myParamPamPam, myParamPamPam, nameof(myParamPamPam));
-			static DateTime Check6(DateTime myParamPamPam) => EnsureArg6.IsGte(myParamPamPam, myParamPamPam);
-			
-			TestBase.AssertThrowsTheSameArgumentException(DateTime.Now, Check, Check6);
+			static DateTime Check(DateTime param1, DateTime param2) => EnsureArg.IsGte(param1, param2, nameof(param1));
+			static DateTime Check6(DateTime param1, DateTime param2) => EnsureArg6.IsGte(param1, param2);
+
+			TestBase.AssertThrowsTheSameArgumentException(Before, After, Check, Check6);
 		}
 
+		/*
 		[Fact]
 		public void IsInRange_Test()
 		{
-			static DateTime Check(DateTime myParamPamPam) => EnsureArg.IsInRange(myParamPamPam, myParamPamPam, myParamPamPam, nameof(myParamPamPam));
-			static DateTime Check6(DateTime myParamPamPam) => EnsureArg6.IsInRange(myParamPamPam, myParamPamPam, myParamPamPam);
-			
-			TestBase.AssertThrowsTheSameArgumentException(DateTime.Now, Check, Check6);
-		}
+			static DateTime Check(DateTime param1, DateTime param2) => EnsureArg.IsInRange(param1, param2, nameof(param1));
+			static DateTime Check6(DateTime param1, DateTime param2) => EnsureArg6.IsInRange(param1, param2);
 
+			TestBase.AssertThrowsTheSameArgumentException(Before, Before, Check, Check6);
+		}
+		*/
 	}
 }
