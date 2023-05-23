2ab8e20ab2ef93a5cea0713bc0c1bfd769b9378b
Remigijus Pankevičius <r-pankevicius@users.noreply.github.com>
2023-05-23 23:02:44 +0300

    Some of EnsureArg6_Types_Tests

--- src/EnsureArg6.Tests/EnsureArg.Types.Tests.cs
+++ src/EnsureArg6.Tests/EnsureArg.Types.Tests.cs
@@ -4,173 +4,151 @@ namespace EnsureArg6.Tests
 {
 	public class EnsureArg6_Types_Tests
 	{
-		//public object IsInt_InstanceTest([NotNull] object param, [CallerArgumentExpression("param")] string paramName = "")
-		//	=> EnsureArg.IsInt(param, paramName);
+		[Fact]
+		public void IsInt_TypeTest()
+		{
+			static Type Check(Type myParamPamPam) => EnsureArg.IsInt(myParamPamPam, nameof(myParamPamPam));
+			static Type Check6(Type myParamPamPam) => EnsureArg6.IsInt(myParamPamPam);
+
+			TestBase.AssertThrowsTheSameArgumentException(typeof(string), Check, Check6);
+		}
+
 		[Fact]
 		public void IsInt_InstanceTest()
 		{
-			static bool Check(bool myParamPamPam) => EnsureArg.IsTrue(myParamPamPam, nameof(myParamPamPam));
-			static bool Check6(bool myParamPamPam) => EnsureArg6.IsTrue(myParamPamPam);
+			static object Check(object myParamPamPam) => EnsureArg.IsInt(myParamPamPam, nameof(myParamPamPam));
+			static object Check6(object myParamPamPam) => EnsureArg6.IsInt(myParamPamPam);
 
-			TestBase.AssertThrowsTheSameArgumentException(false, Check, Check6);
+			TestBase.AssertThrowsTheSameArgumentException((object)"I'm string, not int", Check, Check6);
 		}
 
-		//public Type IsShort_TypeTest([NotNull] Type param, [CallerArgumentExpression("param")] string paramName = "")
-		//	=> EnsureArg.IsShort(param, paramName);
 		[Fact]
 		public void IsShort_TypeTest()
 		{
-			static bool Check(bool myParamPamPam) => EnsureArg.IsTrue(myParamPamPam, nameof(myParamPamPam));
-			static bool Check6(bool myParamPamPam) => EnsureArg6.IsTrue(myParamPamPam);
+			static Type Check(Type myParamPamPam) => EnsureArg.IsShort(myParamPamPam, nameof(myParamPamPam));
+			static Type Check6(Type myParamPamPam) => EnsureArg6.IsShort(myParamPamPam);
 
-			TestBase.AssertThrowsTheSameArgumentException(false, Check, Check6);
+			TestBase.AssertThrowsTheSameArgumentException(typeof(string), Check, Check6);
 		}
 
-		//public object IsShort_InstanceTest([NotNull] object param, [CallerArgumentExpression("param")] string paramName = "")
-		//	=> EnsureArg.IsShort(param, paramName);
 		[Fact]
 		public void IsShort_InstanceTest()
 		{
-			static bool Check(bool myParamPamPam) => EnsureArg.IsTrue(myParamPamPam, nameof(myParamPamPam));
-			static bool Check6(bool myParamPamPam) => EnsureArg6.IsTrue(myParamPamPam);
+			static object Check(object myParamPamPam) => EnsureArg.IsShort(myParamPamPam, nameof(myParamPamPam));
+			static object Check6(object myParamPamPam) => EnsureArg6.IsShort(myParamPamPam);
 
-			TestBase.AssertThrowsTheSameArgumentException(false, Check, Check6);
+			TestBase.AssertThrowsTheSameArgumentException((object)"I'm string", Check, Check6);
 		}
 
-		//public Type IsDecimal_TypeTest([NotNull] Type param, [CallerArgumentExpression("param")] string paramName = "")
-		//	=> EnsureArg.IsDecimal(param, paramName);
 		[Fact]
 		public void IsDecimal_TypeTest()
 		{
-			static bool Check(bool myParamPamPam) => EnsureArg.IsTrue(myParamPamPam, nameof(myParamPamPam));
-			static bool Check6(bool myParamPamPam) => EnsureArg6.IsTrue(myParamPamPam);
+			static Type Check(Type myParamPamPam) => EnsureArg.IsDecimal(myParamPamPam, nameof(myParamPamPam));
+			static Type Check6(Type myParamPamPam) => EnsureArg6.IsDecimal(myParamPamPam);
 
-			TestBase.AssertThrowsTheSameArgumentException(false, Check, Check6);
+			TestBase.AssertThrowsTheSameArgumentException(typeof(string), Check, Check6);
 		}
 
-		//public object IsDecimal_InstanceTest([NotNull] object param, [CallerArgumentExpression("param")] string paramName = "")
-		//	=> EnsureArg.IsDecimal(param, paramName);
 		[Fact]
 		public void IsDecimal_InstanceTest()
 		{
-			static bool Check(bool myParamPamPam) => EnsureArg.IsTrue(myParamPamPam, nameof(myParamPamPam));
-			static bool Check6(bool myParamPamPam) => EnsureArg6.IsTrue(myParamPamPam);
+			static object Check(object myParamPamPam) => EnsureArg.IsDecimal(myParamPamPam, nameof(myParamPamPam));
+			static object Check6(object myParamPamPam) => EnsureArg6.IsDecimal(myParamPamPam);
 
-			TestBase.AssertThrowsTheSameArgumentException(false, Check, Check6);
+			TestBase.AssertThrowsTheSameArgumentException((object)"I'm string", Check, Check6);
 		}
 
-		//public Type IsDouble_TypeTest([NotNull] Type param, [CallerArgumentExpression("param")] string paramName = "")
-		//	=> EnsureArg.IsDouble(param, paramName);
 		[Fact]
 		public void IsDouble_TypeTest()
 		{
-			static bool Check(bool myParamPamPam) => EnsureArg.IsTrue(myParamPamPam, nameof(myParamPamPam));
-			static bool Check6(bool myParamPamPam) => EnsureArg6.IsTrue(myParamPamPam);
+			static Type Check(Type myParamPamPam) => EnsureArg.IsDouble(myParamPamPam, nameof(myParamPamPam));
+			static Type Check6(Type myParamPamPam) => EnsureArg6.IsDouble(myParamPamPam);
 
-			TestBase.AssertThrowsTheSameArgumentException(false, Check, Check6);
+			TestBase.AssertThrowsTheSameArgumentException(typeof(string), Check, Check6);
 		}
 
-		//public object IsDouble_InstanceTest([NotNull] object param, [CallerArgumentExpression("param")] string paramName = "")
-		//	=> EnsureArg.IsDouble(param, paramName);
 		[Fact]
 		public void IsDouble_InstanceTest()
 		{
-			static bool Check(bool myParamPamPam) => EnsureArg.IsTrue(myParamPamPam, nameof(myParamPamPam));
-			static bool Check6(bool myParamPamPam) => EnsureArg6.IsTrue(myParamPamPam);
+			static object Check(object myParamPamPam) => EnsureArg.IsDouble(myParamPamPam, nameof(myParamPamPam));
+			static object Check6(object myParamPamPam) => EnsureArg6.IsDouble(myParamPamPam);
 
-			TestBase.AssertThrowsTheSameArgumentException(false, Check, Check6);
+			TestBase.AssertThrowsTheSameArgumentException((object)"I'm string", Check, Check6);
 		}
 
-		//public Type IsFloat_TypeTest([NotNull] Type param, [CallerArgumentExpression("param")] string paramName = "")
-		//	=> EnsureArg.IsFloat(param, paramName);
 		[Fact]
 		public void IsFloat_TypeTest()
 		{
-			static bool Check(bool myParamPamPam) => EnsureArg.IsTrue(myParamPamPam, nameof(myParamPamPam));
-			static bool Check6(bool myParamPamPam) => EnsureArg6.IsTrue(myParamPamPam);
+			static Type Check(Type myParamPamPam) => EnsureArg.IsFloat(myParamPamPam, nameof(myParamPamPam));
+			static Type Check6(Type myParamPamPam) => EnsureArg6.IsFloat(myParamPamPam);
 
-			TestBase.AssertThrowsTheSameArgumentException(false, Check, Check6);
+			TestBase.AssertThrowsTheSameArgumentException(typeof(string), Check, Check6);
 		}
 
-		//public object IsFloat_InstanceTest([NotNull] object param, [CallerArgumentExpression("param")] string paramName = "")
-		//	=> EnsureArg.IsFloat(param, paramName);
 		[Fact]
 		public void IsFloat_InstanceTest()
 		{
-			static bool Check(bool myParamPamPam) => EnsureArg.IsTrue(myParamPamPam, nameof(myParamPamPam));
-			static bool Check6(bool myParamPamPam) => EnsureArg6.IsTrue(myParamPamPam);
+			static object Check(object myParamPamPam) => EnsureArg.IsFloat(myParamPamPam, nameof(myParamPamPam));
+			static object Check6(object myParamPamPam) => EnsureArg6.IsFloat(myParamPamPam);
 
-			TestBase.AssertThrowsTheSameArgumentException(false, Check, Check6);
+			TestBase.AssertThrowsTheSameArgumentException((object)"I'm string", Check, Check6);
 		}
 
-		//public Type IsBool_TypeTest([NotNull] Type param, [CallerArgumentExpression("param")] string paramName = "")
-		//	=> EnsureArg.IsBool(param, paramName);
 		[Fact]
 		public void IsBool_TypeTest()
 		{
-			static bool Check(bool myParamPamPam) => EnsureArg.IsTrue(myParamPamPam, nameof(myParamPamPam));
-			static bool Check6(bool myParamPamPam) => EnsureArg6.IsTrue(myParamPamPam);
+			static Type Check(Type myParamPamPam) => EnsureArg.IsBool(myParamPamPam, nameof(myParamPamPam));
+			static Type Check6(Type myParamPamPam) => EnsureArg6.IsBool(myParamPamPam);
 
-			TestBase.AssertThrowsTheSameArgumentException(false, Check, Check6);
+			TestBase.AssertThrowsTheSameArgumentException(typeof(string), Check, Check6);
 		}
 
-		//public object IsBool_InstanceTest([NotNull] object param, [CallerArgumentExpression("param")] string paramName = "")
-		//	=> EnsureArg.IsBool(param, paramName);
 		[Fact]
 		public void IsBool_InstanceTest()
 		{
-			static bool Check(bool myParamPamPam) => EnsureArg.IsTrue(myParamPamPam, nameof(myParamPamPam));
-			static bool Check6(bool myParamPamPam) => EnsureArg6.IsTrue(myParamPamPam);
+			static object Check(object myParamPamPam) => EnsureArg.IsBool(myParamPamPam, nameof(myParamPamPam));
+			static object Check6(object myParamPamPam) => EnsureArg6.IsBool(myParamPamPam);
 
-			TestBase.AssertThrowsTheSameArgumentException(false, Check, Check6);
+			TestBase.AssertThrowsTheSameArgumentException((object)"I'm string", Check, Check6);
 		}
 
-		//public Type IsDateTime_TypeTest([NotNull] Type param, [CallerArgumentExpression("param")] string paramName = "")
-		//	=> EnsureArg.IsDateTime(param, paramName);
 		[Fact]
 		public void IsDateTime_TypeTest()
 		{
-			static bool Check(bool myParamPamPam) => EnsureArg.IsTrue(myParamPamPam, nameof(myParamPamPam));
-			static bool Check6(bool myParamPamPam) => EnsureArg6.IsTrue(myParamPamPam);
+			static Type Check(Type myParamPamPam) => EnsureArg.IsDateTime(myParamPamPam, nameof(myParamPamPam));
+			static Type Check6(Type myParamPamPam) => EnsureArg6.IsDateTime(myParamPamPam);
 
-			TestBase.AssertThrowsTheSameArgumentException(false, Check, Check6);
+			TestBase.AssertThrowsTheSameArgumentException(typeof(string), Check, Check6);
 		}
 
-		//public object IsDateTime_InstanceTest([NotNull] object param, [CallerArgumentExpression("param")] string paramName = "")
-		//	=> EnsureArg.IsDateTime(param, paramName);
 		[Fact]
 		public void IsDateTime_InstanceTest()
 		{
-			static bool Check(bool myParamPamPam) => EnsureArg.IsTrue(myParamPamPam, nameof(myParamPamPam));
-			static bool Check6(bool myParamPamPam) => EnsureArg6.IsTrue(myParamPamPam);
+			static object Check(object myParamPamPam) => EnsureArg.IsDateTime(myParamPamPam, nameof(myParamPamPam));
+			static object Check6(object myParamPamPam) => EnsureArg6.IsDateTime(myParamPamPam);
 
-			TestBase.AssertThrowsTheSameArgumentException(false, Check, Check6);
+			TestBase.AssertThrowsTheSameArgumentException((object)"I'm string", Check, Check6);
 		}
 
-		//public Type IsString_TypeTest([NotNull] Type param, [CallerArgumentExpression("param")] string paramName = "")
-		//	=> EnsureArg.IsString(param, paramName);
 		[Fact]
 		public void IsString_TypeTest()
 		{
-			static bool Check(bool myParamPamPam) => EnsureArg.IsTrue(myParamPamPam, nameof(myParamPamPam));
-			static bool Check6(bool myParamPamPam) => EnsureArg6.IsTrue(myParamPamPam);
+			static Type Check(Type myParamPamPam) => EnsureArg.IsString(myParamPamPam, nameof(myParamPamPam));
+			static Type Check6(Type myParamPamPam) => EnsureArg6.IsString(myParamPamPam);
 
-			TestBase.AssertThrowsTheSameArgumentException(false, Check, Check6);
+			TestBase.AssertThrowsTheSameArgumentException(typeof(bool), Check, Check6);
 		}
 
-		//public object IsString_InstanceTest([NotNull] object param, [CallerArgumentExpression("param")] string paramName = "")
-		//	=> EnsureArg.IsString(param, paramName);
 		[Fact]
 		public void IsString_InstanceTest()
 		{
-			static bool Check(bool myParamPamPam) => EnsureArg.IsTrue(myParamPamPam, nameof(myParamPamPam));
-			static bool Check6(bool myParamPamPam) => EnsureArg6.IsTrue(myParamPamPam);
+			static object Check(object myParamPamPam) => EnsureArg.IsString(myParamPamPam, nameof(myParamPamPam));
+			static object Check6(object myParamPamPam) => EnsureArg6.IsString(myParamPamPam);
 
-			TestBase.AssertThrowsTheSameArgumentException(false, Check, Check6);
+			TestBase.AssertThrowsTheSameArgumentException((object)1, Check, Check6);
 		}
 
-		//public object IsOfType_Test([NotNull] object param, Type expectedType, [CallerArgumentExpression("param")] string paramName = "")
-		//	=> EnsureArg.IsOfType(param, expectedType, paramName);
+		// ???????????????
 		[Fact]
 		public void IsOfType_Test()
 		{
