fa80fb4211490223c1f7d69f2cdbeed5b66b90a5
Remigijus Pankevičius <r-pankevicius@users.noreply.github.com>
2023-05-29 22:06:32 +0300

    Moving on EnsurEnsureArg6_Strings_Tests

--- src/EnsureArg6.Tests/EnsureArg.Strings.Tests.cs
+++ src/EnsureArg6.Tests/EnsureArg.Strings.Tests.cs
@@ -1,9 +1,14 @@
 ﻿using EnsureThat;
+using System.Text.RegularExpressions;
 
 namespace EnsureArg6.Tests
 {
 	public class EnsurEnsureArg6_Strings_Tests
 	{
+		private const string AllDigitsPatternString = "^[0-9]+$";
+
+		private static readonly Regex AllDigitsRegex = new(AllDigitsPatternString);
+
 		[Fact]
 		public void IsNotNull_Test()
 		{
@@ -34,14 +39,66 @@ namespace EnsureArg6.Tests
 			TestBase.AssertThrowsTheSameArgumentException(param, Check, Check6);
 		}
 
-		// 
-		// 
-		// IsNotEmptyOrWhiteSpace
-		// IsNotEmpty
-		// HasLength
-		// HasLengthBetween
-		// Matches_StringPattern
-		// Matches_Regex
+		[Fact]
+		public void IsNotEmptyOrWhiteSpace_Test()
+		{
+			static string Check(string? myParamPamPam) => EnsureArg.IsNotEmptyOrWhiteSpace(myParamPamPam, nameof(myParamPamPam));
+			static string Check6(string? myParamPamPam) => EnsureArg6.IsNotEmptyOrWhiteSpace(myParamPamPam);
+
+			string? param = "    ";
+			TestBase.AssertThrowsTheSameArgumentException(param, Check, Check6);
+		}
+
+		[Fact]
+		public void IsNotEmpty_Test()
+		{
+			static string Check(string? myParamPamPam) => EnsureArg.IsNotEmpty(myParamPamPam, nameof(myParamPamPam));
+			static string Check6(string? myParamPamPam) => EnsureArg6.IsNotEmpty(myParamPamPam);
+
+			string? param = "";
+			TestBase.AssertThrowsTheSameArgumentException(param, Check, Check6);
+		}
+
+		[Fact]
+		public void HasLength_Test()
+		{
+			static string Check(string? myParamPamPam) => EnsureArg.HasLength(myParamPamPam, 3, nameof(myParamPamPam));
+			static string Check6(string? myParamPamPam) => EnsureArg6.HasLength(myParamPamPam, 3);
+
+			string? param = "AAA4";
+			TestBase.AssertThrowsTheSameArgumentException(param, Check, Check6);
+		}
+
+		[Fact]
+		public void HasLengthBetween_Test()
+		{
+			static string Check(string? myParamPamPam) => EnsureArg.HasLengthBetween(myParamPamPam, 3, 4, nameof(myParamPamPam));
+			static string Check6(string? myParamPamPam) => EnsureArg6.HasLengthBetween(myParamPamPam, 3, 4);
+
+			string? param = "AAA45";
+			TestBase.AssertThrowsTheSameArgumentException(param, Check, Check6);
+		}
+
+		[Fact]
+		public void Matches_StringPattern_Test()
+		{
+			static string Check(string? myParamPamPam) => EnsureArg.Matches(myParamPamPam, AllDigitsPatternString, nameof(myParamPamPam));
+			static string Check6(string? myParamPamPam) => EnsureArg6.Matches(myParamPamPam, AllDigitsPatternString);
+
+			string? param = "4A";
+			TestBase.AssertThrowsTheSameArgumentException(param, Check, Check6);
+		}
+
+		[Fact]
+		public void Matches_Regex_Test()
+		{
+			static string Check(string? myParamPamPam) => EnsureArg.Matches(myParamPamPam, AllDigitsRegex, nameof(myParamPamPam));
+			static string Check6(string? myParamPamPam) => EnsureArg6.Matches(myParamPamPam, AllDigitsRegex);
+
+			string? param = "4A";
+			TestBase.AssertThrowsTheSameArgumentException(param, Check, Check6);
+		}
+
 		// Is
 		// Is_WithStringComparison
 		// IsEqualTo
